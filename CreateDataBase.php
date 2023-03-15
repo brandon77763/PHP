c9688e664a2ceb17b32e52e46a62bc4583044d9a
brandon77763 <52871954+brandon77763@users.noreply.github.com>
2023-03-15 04:14:16 -0400

    Create CreateDataBase.php

---
+++ CreateDataBase.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "mydb";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+
+// Create database
+$sql = "CREATE DATABASE myDB";
+if (mysqli_query($conn, $sql)) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . mysqli_error($conn);
+}
+
+// Close connection
+mysqli_close($conn);
+
+?>
